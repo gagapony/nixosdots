bb81bf60148943b1253ec21d9b62d66a756431e7
gagapony <aoengo@outlook.com>
2026-07-06 11:12:31 +0800

    update nixos config

--- modules/programs/wechat.nix
+++ modules/programs/wechat.nix
@@ -1,23 +1,23 @@
 { pkgs, ... }:
 let
   wechatDesktop = pkgs.makeDesktopItem {
-    name = "com.tencent.wechat"; # 这会生成 com.tencent.wechat.desktop
+    name = "wechat"; # 这会生成 wechat.desktop
     desktopName = "WeChat";
     genericName = "WeChat Client";
-    exec = "${pkgs.wechat-uos}/bin/wechat-uos --enable-features=UseOzonePlatform --ozone-platform=wayland -- %U";
-    icon = "com.tencent.wechat";
+    exec = "${pkgs.wechat}/bin/wechat --enable-features=UseOzonePlatform --ozone-platform=wayland -- %U";
+    icon = "wechat";
     comment = "WeChat Client";
     categories = [ "Utility" ];
     terminal = false;
     startupNotify = true;
-    startupWMClass = "wechat-uos";
+    startupWMClass = "wechat";
   };
 
   _wechat = pkgs.symlinkJoin {
-    name = "wechat-uos";
-    paths = [ pkgs.wechat-uos ];
+    name = "wechat";
+    paths = [ pkgs.wechat ];
     postBuild = ''
-      rm $out/share/applications/com.tencent.wechat.desktop
+      rm -f $out/share/applications/wechat.desktop
       cp ${wechatDesktop}/share/applications/* $out/share/applications/
     '';
   };
